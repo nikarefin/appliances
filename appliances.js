eaa6630d96f9a493f279bc6213b7ca0b05d2d7d6
Nik Arefin <nikarefin@gmail.com>
2024-02-20 02:26:56 +0300

    Initial commit

---
+++ appliances.js
@@ -0,0 +1,76 @@
+class Appliance {
+    constructor(appliance, power) {
+        this.appliance = appliance;
+        this.power = power;
+        this.isPlugedIn = false;
+    }
+    plugIn() {
+        console.log(`${this.appliance} включен в розетку`)
+        return this.isPlugedIn = true
+    }
+    unplug() {
+        console.log(`${this.appliance} выключен из розетки`)
+        return this.isPlugedIn = false
+    }
+}
+
+class Lamp extends Appliance {
+    constructor(type) {
+        super();
+        this.type = type;
+    }
+    appliance = 'Лампа';
+}
+
+class TV extends Appliance {
+    constructor(size, matrix) {
+        super();
+        this.size = size;
+        this.matrix = matrix;
+    }
+    turnOffOnTimer(time) {
+        console.log(`Телевизор выключится через ${time} минут`)
+    }
+    appliance = 'Телевизор';
+}
+
+class Kettle extends Appliance {
+    constructor(capacity, color) {
+        super();
+        this.capacity = capacity;
+        this.color = color;
+    }
+    appliance = 'Чайник';
+}
+
+function showCommonPower(appliances) {
+    let commonPower = 0;
+    for (let item in appliances) {
+        if (appliances[item].isPlugedIn) {
+            commonPower += appliances[item].power
+        }
+    }
+    console.log('_________________________________')
+    console.log(`Общая потребляемая мощность: ${commonPower} Вт`)
+}
+
+let lamp = new Lamp();
+lamp.power = 60;
+lamp.type = 'LED';
+lamp.plugIn();
+
+let tv = new TV();
+tv.power = 120;
+tv.size = '55';
+tv.matrix = 'OLED';
+tv.plugIn();
+
+let kettle = new Kettle();
+kettle.power = 100;
+kettle.capacity = 1.5;
+kettle.color = 'White';
+kettle.unplug()
+
+let myAppliances = [lamp, tv, kettle];
+
+showCommonPower(myAppliances)
